cedd658b31a5f54243911a91c5f8aa17431527f6
cesar <cesar.ortiz@mail.escuelaing.edu.co>
2020-10-09 22:30:54 -0500

    subiendo

--- src/main/java/edu/eci/cvds/view/AlquilerBean.java
+++ src/main/java/edu/eci/cvds/view/AlquilerBean.java
@@ -21,80 +21,80 @@ import java.sql.Date;
 @ApplicationScoped
 public class AlquilerBean extends BasePageBean {
 
-    @Inject
-    private ServiciosAlquiler serviciosAlquiler;
-    private Cliente selectedCliente;
-	private long costo;
-    public List<Cliente> consultarClientes(){
-        List<Cliente> clientes = null;
-        try{
-            clientes=serviciosAlquiler.consultarClientes();
-        } catch (ExcepcionServiciosAlquiler e) {
-            setErrorMessage(e);
-        }
-        return clientes;
-    }
-    public Cliente consultarCliente(long documento){
-        Cliente cliente=null;
-        try {
-            cliente=serviciosAlquiler.consultarCliente(documento);
-        } catch (Exception e) {
-            setErrorMessage(e);
-        }
-        return cliente;
-    }
-    public void registrarCliente(long doc,String nombre,String telefono, String direccion,String mail){
-        try{
-            serviciosAlquiler.registrarCliente(new Cliente(nombre,doc,telefono,direccion,mail));
-        } catch (Exception e) {
-            setErrorMessage(e);
-        }
-    }
+	 @Inject
+	    private ServiciosAlquiler serviciosAlquiler;
+	    private Cliente selectedCliente;
+		private long costo;
+	    public List<Cliente> consultarClientes(){
+	        List<Cliente> clientes = null;
+	        try{
+	            clientes=serviciosAlquiler.consultarClientes();
+	        } catch (ExcepcionServiciosAlquiler e) {
+	            setErrorMessage(e);
+	        }
+	        return clientes;
+	    }
+	    public Cliente consultarCliente(long documento){
+	        Cliente cliente=null;
+	        try {
+	            cliente=serviciosAlquiler.consultarCliente(documento);
+	        } catch (Exception e) {
+	            setErrorMessage(e);
+	        }
+	        return cliente;
+	    }
+	    public void registrarCliente(long doc,String nombre,String telefono, String direccion,String mail){
+	        try{
+	            serviciosAlquiler.registrarCliente(new Cliente(nombre,doc,telefono,direccion,mail));
+	        } catch (Exception e) {
+	            setErrorMessage(e);
+	        }
+	    }
 
-    public void setSelectedCliente(Cliente cliente){this.selectedCliente = cliente;}
+	    public void setSelectedCliente(Cliente cliente){this.selectedCliente = cliente;}
 
-    public Cliente getSelectedCliente(){
-        return selectedCliente;
-    }
-	
-	
-	public long consultarMulta(Item it) {
-		long multa = 0;
-		try{
-			multa= serviciosAlquiler.consultarMultaAlquiler(it.getId(),new Date(System.currentTimeMillis()));
-		}
-		catch(ExcepcionServiciosAlquiler e){
-            setErrorMessage(e);
-        }
-		return multa;
-    }
-	
-	
-	public void registrarAlquilerCliente(int id,int numDiasAlquilar){
-        try{
-            Item item = serviciosAlquiler.consultarItem(id);
-            serviciosAlquiler.registrarAlquilerCliente(new Date(System.currentTimeMillis()),selectedCliente.getDocumento(),item,numDiasAlquilar);
-        }catch(ExcepcionServiciosAlquiler e){
-            setErrorMessage(e);
-        }
-    }
-	
-	public void consultarCosto(int id, int numDiasAlquilar){
-        try {
-            costo = serviciosAlquiler.consultarCostoAlquiler(id, numDiasAlquilar);
-        } catch (ExcepcionServiciosAlquiler e){
-            setErrorMessage(e);
-        }
-    }
-	
-	public long getCosto(){
-        return costo;
-    }
+	    public Cliente getSelectedCliente(){
+	        return selectedCliente;
+	    }
+		
+		
+		public long consultarMulta(Item it) {
+			long multa = 0;
+			try{
+				multa= serviciosAlquiler.consultarMultaAlquiler(it.getId(),new Date(System.currentTimeMillis()));
+			}
+			catch(ExcepcionServiciosAlquiler e){
+	            setErrorMessage(e);
+	        }
+			return multa;
+	    }
+		
+		
+		public void registrarAlquilerCliente(int id,int numDiasAlquilar){
+	        try{
+	            Item item = serviciosAlquiler.consultarItem(id);
+	            serviciosAlquiler.registrarAlquilerCliente(new Date(System.currentTimeMillis()),selectedCliente.getDocumento(),item,numDiasAlquilar);
+	        }catch(ExcepcionServiciosAlquiler e){
+	            setErrorMessage(e);
+	        }
+	    }
+		
+		public void consultarCosto(int id, int numDiasAlquilar){
+	        try {
+	            costo = serviciosAlquiler.consultarCostoAlquiler(id, numDiasAlquilar);
+	        } catch (ExcepcionServiciosAlquiler e){
+	            setErrorMessage(e);
+	        }
+	    }
+		
+		public long getCosto(){
+	        return costo;
+	    }
 
-    private void setErrorMessage(Exception e){
-        String message = e.getMessage();
-        FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_INFO, message, null));
-    }
+	    private void setErrorMessage(Exception e){
+	        String message = e.getMessage();
+	        FacesContext.getCurrentInstance().addMessage(null,
+	                new FacesMessage(FacesMessage.SEVERITY_INFO, message, null));
+	    }
 
 }
