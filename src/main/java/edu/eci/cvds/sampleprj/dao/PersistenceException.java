cf4b5fef0234580f1a3a2185b4ac4e354b535ecc
Laura Izquierdo <alizeci17@gmail.com>
2020-10-09 20:35:47 -0500

    ajuste

--- src/main/java/edu/eci/cvds/sampleprj/dao/PersistenceException.java
+++
@@ -1,9 +0,0 @@
-package edu.eci.cvds.sampleprj.dao;
-
-public class PersistenceException extends Exception{
-	
-	public PersistenceException (String mensaje, org.apache.ibatis.exceptions.PersistenceException e) {
-		super(mensaje);
-	}
-	
-}
