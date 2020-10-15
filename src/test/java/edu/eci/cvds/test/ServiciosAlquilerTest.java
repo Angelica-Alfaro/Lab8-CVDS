499159e9dc1d44459e0859ae2c4d96e2c3bdcc76
Cesar Fernando Ortiz Rocha <cesar.ortiz@mail.escuelaing.edu.co>
2020-10-15 01:27:51 -0500

    Finalizando Laboratorio2

--- src/test/java/edu/eci/cvds/test/ServiciosAlquilerTest.java
+++ src/test/java/edu/eci/cvds/test/ServiciosAlquilerTest.java
@@ -1,24 +1,20 @@
-
+﻿
 
 	package edu.eci.cvds.test;
 
 	import java.sql.Date;
 	import java.text.SimpleDateFormat;
 	import java.time.LocalDate;
-	import java.util.ArrayList;
-	import java.util.List;
 
 	import com.google.inject.Inject;
 	import edu.eci.cvds.sampleprj.dao.PersistenceException;
 	import edu.eci.cvds.samples.entities.Cliente;
 	import edu.eci.cvds.samples.entities.Item;
-	import edu.eci.cvds.samples.entities.ItemRentado;
 	import edu.eci.cvds.samples.entities.TipoItem;
 	import edu.eci.cvds.samples.services.ExcepcionServiciosAlquiler;
 	import edu.eci.cvds.samples.services.ServiciosAlquiler;
 	import edu.eci.cvds.samples.services.ServiciosAlquilerFactory;
 	import org.apache.ibatis.session.SqlSession;
-	import org.junit.Before;
 	import org.junit.Test;
 	import org.junit.Assert;
 
